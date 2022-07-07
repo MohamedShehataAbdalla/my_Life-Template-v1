6a8a19cc8db133eab4b3fb5f99bb57d373dbab06
MohamedShehataAbdalla <moh.shehata143@gmail.com>
2022-07-07 23:51:26 +0200

    Upload All Files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# my_Life-Website
+# my_Life-Template-v1
 HTML5 &amp; CSS3 &amp; JS &amp; Responsive Not Freamwork
--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,30 @@
-/* global $ */
 
-$(function () {
-  'use strict';
-  $('.info-list li').click(function (){
-    $(this).addClass('selected').siblings('li').removeClass('selected');
+// $(function () {
+//   'use strict';
+//   $('.info-list li').click(function (){
+//     $(this).addClass('selected').siblings('li').removeClass('selected');
+//   });
+// });
+
+let list = document.querySelectorAll('.information ul li'),
+    div  = document.querySelectorAll('.information .info-content > div');
+list.forEach(ele => {
+  ele.addEventListener('click', function(e){
+    list.forEach(ele => {
+      ele.classList.remove('selected');
+    });
+    e.target.classList.add('selected');
+
+    div.forEach(ele => {
+      ele.style.display = 'none';
+    })
+
+    document.querySelectorAll(e.target.dataset.class).forEach(ele => {
+      ele.style.display = 'block';
+    })
+
+    
+
+
   });
 });
